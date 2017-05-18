635fe3509a3476bd9bd0426b001961a43523a7db
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-05-18 11:36:14 +0100

    updated JS

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -16,7 +16,7 @@ BasicGame.MainMenu.prototype = {
 		starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
 		logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
 		logo.anchor.setTo(0.5, 0.5);
-		startButton = this.add.button((this.world.width / 2),  (this.world.height / 2) + 50, 'startButton'), this.startGame);
+		startButton = this.add.button((this.world.width / 2),  (this.world.height / 2) + 50, 'startButton', this.startGame);
 		startButton.anchor.setTo(0.5, 0.5);
 	},
 
