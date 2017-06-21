ae401401772d87b3a9c263797b389b93b3840b9c
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-06-21 10:16:41 +0100

    stage 1

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -8,11 +8,9 @@ BasicGame.MainMenu.prototype = {
 
 	create: function () {
 
-		//We've already preloded our assers, so let's kick right into the Main Menu itself.
-		//Here all we're doing is playing same music and adding a picture and button
 		//Outputting sky, ship, score, lives, total and Start Time to the screen
 		//The scrolling starfield background
-		starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+		starfield = this.ass.tileSprite(0, 0, 800, 600, 'starfield');
 		logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
 		logo.anchor.setTo(0.5,0.5);
 		startButton = this.add.button((this.world.width / 2), (this.world.height / 2) + 50, 'startButton', this.startGame);
@@ -24,7 +22,6 @@ BasicGame.MainMenu.prototype = {
 	},
 
 	startGame: function () {
-		//And start the actual game
 		this.game.state.start('Game');
 	}
 
