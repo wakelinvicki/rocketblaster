d7ce2cd0c58e5e995b4ca76473eb4f63a062de65
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-06-21 10:20:41 +0100

    removed ass

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -10,7 +10,7 @@ BasicGame.MainMenu.prototype = {
 
 		//Outputting sky, ship, score, lives, total and Start Time to the screen
 		//The scrolling starfield background
-		starfield = this.ass.tileSprite(0, 0, 800, 600, 'starfield');
+		starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
 		logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) - 150, 'logo');
 		logo.anchor.setTo(0.5,0.5);
 		startButton = this.add.button((this.world.width / 2), (this.world.height / 2) + 50, 'startButton', this.startGame);
