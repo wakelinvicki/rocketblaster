bbe41fb24d9a3fccb37cbfc34d81f03bccb7c30d
wakelinvicki <w1618838@student.southstaffs.ac.uk>
2017-06-23 10:12:14 +0100

    logo change

--- scripts/Preloader.js
+++ scripts/Preloader.js
@@ -16,7 +16,7 @@ align: 'center'
 this.preloaderText.anchor.setTo(0.5, 0.5);
 
 //preload the images, sprites and audio assets into memory
-this.load.image('logo', 'assets/PhaserLogo.png');
+this.load.image('logo', 'assets/logo.png');
 this.load.image('starfield', 'assets/starfield.png');
 this.load.image('startButton', 'assets/startButton.png');
 this.load.image('ship', 'assets/ship.png');
